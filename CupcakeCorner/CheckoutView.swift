6672c151f1aa5eb2b2ee9c42c087b1ed55828a11
Vahtee Boo <vahtee.boo@gmail.com>
2022-01-25 14:15:21 +0200

    Release candidate

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -10,13 +10,15 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var body: some View {
         ScrollView {
             VStack {
-            
-//                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) {
+                
                 AsyncImage(url: URL(string: "https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/unicorn_cupcakes_67877_16x9.jpg"), scale: 3) {
-                image in
+                    image in
                     image
                         .resizable()
                         .scaledToFit()
@@ -29,13 +31,42 @@ struct CheckoutView: View {
                     .font(.title)
                 
                 Button("Place Order") {
-                    
+                    Task {
+                        await placeOrder()
+                    }
                 }
                 .padding()
             }
         }
         .navigationTitle("Check out")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("OK") {}
+        } message: {
+            Text(confirmationMessage)
+        }
+    }
+    
+    func placeOrder() async {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Conten-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed.")
+        }
     }
 }
 
